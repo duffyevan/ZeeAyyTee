f7ad390b93366285b073b56e3e0de0ec980da496
Evan Duffy <eduffy2015@spyponders.com>
2016-07-01 12:19:22 -0400

    Line 0x40-0x4f
    Added all to a new file to store while creating new lines

---
+++ append to code.c
@@ -0,0 +1,160 @@
+	case 0x30:
+		//jr nc,*
+		
+		return 0;
+
+	case 0x31:
+		//ld sp,**
+		
+		return 0;
+
+	case 0x32:
+		//ld (**),a
+		
+		return 0;
+
+	case 0x33:
+		//inc sp
+		
+		return 0;
+
+	case 0x34:
+		//inc (hl)
+		
+		return 0;
+
+	case 0x35:
+		//dec (hl)
+		
+		return 0;
+
+	case 0x36:
+		//ld (hl),*
+		
+		return 0;
+
+	case 0x37:
+		//scf
+		
+		return 0;
+
+	case 0x38:
+		//jr c,*
+		
+		return 0;
+
+	case 0x39:
+		//add hl,sp
+		
+		return 0;
+
+	case 0x3a:
+		//ld a,(**)
+		
+		return 0;
+
+	case 0x3b:
+		//dec sp
+		
+		return 0;
+
+	case 0x3c:
+		//inc a
+		
+		return 0;
+
+	case 0x3d:
+		//dec a
+		
+		return 0;
+
+	case 0x3e:
+		//ld a,*
+		
+		return 0;
+
+	case 0x3f:
+		//ccf
+		
+		return 0;
+
+	case 0x40:
+		//ld b,b
+		
+		return 0;
+
+	case 0x41:
+		//ld b,c
+		
+		return 0;
+
+	case 0x42:
+		//ld b,d
+		
+		return 0;
+
+	case 0x43:
+		//ld b,e
+		
+		return 0;
+
+	case 0x44:
+		//ld b,h
+		
+		return 0;
+
+	case 0x45:
+		//ld b,l
+		
+		return 0;
+
+	case 0x46:
+		//ld b, (hl)
+		
+		return 0;
+
+	case 0x47:
+		//ld b,a
+		
+		return 0;
+
+	case 0x48:
+		//ld c,b
+		
+		return 0;
+
+	case 0x49:
+		//ld c,c
+		
+		return 0;
+
+	case 0x4a:
+		//ld c,d
+		
+		return 0;
+
+	case 0x4b:
+		//ld c,e
+		
+		return 0;
+
+	case 0x4c:
+		//ld c,h
+		
+		return 0;
+
+	case 0x4d:
+		//ld c,l
+		
+		return 0;
+
+	case 0x4e:
+		//ld c,(hl)
+		
+		return 0;
+
+	case 0x4f:
+		//ld c,a
+		
+		return 0;
+
